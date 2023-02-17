12750f70a4c1837eeeeeb4adbcdcd26449e31631
GodlyHamster <wouter.van.dompselaar@gmail.com>
2023-02-17 11:01:34 +0100

    compass points at waypoint

--- BBKoffieTuin/Assets/Scripts/PhoneDirection.cs
+++ BBKoffieTuin/Assets/Scripts/PhoneDirection.cs
@@ -7,7 +7,10 @@ public class PhoneDirection : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI directionDebug;
     [SerializeField] private TextMeshProUGUI accuracyDebug;
+    [SerializeField] private TextMeshProUGUI nextPointDebug;
     [SerializeField] private RectTransform windrose;
+    private Vector2 _currentPos = new Vector2(4.856711f, 52.390652f);
+    private Vector2 _nextPos = new Vector2(4.856711f, 52.390652f);
 
     private void Awake()
     {
@@ -22,9 +25,23 @@ public class PhoneDirection : MonoBehaviour
         directionDebug.text = headingDegrees.ToString();
         accuracyDebug.text = headingAccuracy.ToString();
 
-        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, headingDegrees));
+        Vector2 lookDirection = (_nextPos - _currentPos).normalized;
+
+        float directionInDegrees = Mathf.Atan2(lookDirection.y, lookDirection.x);
+
+        directionInDegrees = directionInDegrees * 180 / Mathf.PI;
+
+        directionInDegrees = (directionInDegrees - 90) * -1;
+
+        nextPointDebug.text = directionInDegrees.ToString();
+
+        directionInDegrees += headingDegrees;
         
-        float rotationSpeed = (targetRotation.eulerAngles - windrose.eulerAngles).magnitude;
-        windrose.rotation = Quaternion.RotateTowards(windrose.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        windrose.rotation = Quaternion.Euler(0, 0, directionInDegrees);
+
+        // Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, headingDegrees));
+        //
+        // float rotationSpeed = (targetRotation.eulerAngles - windrose.eulerAngles).magnitude;
+        // windrose.rotation = Quaternion.RotateTowards(windrose.rotation, targetRotation, Time.deltaTime * rotationSpeed);
     }
 }
