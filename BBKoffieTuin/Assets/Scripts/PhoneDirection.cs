f3dd6e1d82ebdbb0c008c631e14ff43092debaf6
GodlyHamster <wouter.van.dompselaar@gmail.com>
2023-02-20 12:14:13 +0100

    pointer points towards waypoint

--- BBKoffieTuin/Assets/Scripts/PhoneDirection.cs
+++ BBKoffieTuin/Assets/Scripts/PhoneDirection.cs
@@ -8,8 +8,13 @@ public class PhoneDirection : MonoBehaviour
     [SerializeField] private TextMeshProUGUI directionDebug;
     [SerializeField] private TextMeshProUGUI accuracyDebug;
     [SerializeField] private TextMeshProUGUI nextPointDebug;
+    [SerializeField] private TextMeshProUGUI currentLocationDebug;
+    [SerializeField] private TextMeshProUGUI nextLocationDebug;
+    [SerializeField] private TextMeshProUGUI distanceDebug;
+    
     [SerializeField] private RectTransform windrose;
-    private Vector2 _currentPos = new Vector2(4.856711f, 52.390652f);
+    [SerializeField] private RectTransform pointer;
+    private Vector2 _currentPos;
     private Vector2 _nextPos = new Vector2(4.856711f, 52.390652f);
 
     private void Awake()
@@ -19,10 +24,21 @@ public class PhoneDirection : MonoBehaviour
 
     void Update()
     {
-        float headingDegrees = Input.compass.magneticHeading;
-        float headingAccuracy = Input.compass.headingAccuracy;
+        float latitude = Input.location.lastData.latitude;
+        float longitude = Input.location.lastData.longitude;
         
-        directionDebug.text = headingDegrees.ToString();
+        float northHeadingDegrees = Input.compass.magneticHeading;
+        float headingAccuracy = Input.compass.headingAccuracy;
+
+        float distance = (_nextPos - _currentPos).magnitude;
+
+        _currentPos = new Vector2(longitude, latitude);
+
+        currentLocationDebug.text = _currentPos.ToString();
+        nextLocationDebug.text = _nextPos.ToString();
+        distanceDebug.text = (_nextPos - _currentPos).magnitude.ToString();
+        
+        directionDebug.text = northHeadingDegrees.ToString();
         accuracyDebug.text = headingAccuracy.ToString();
 
         Vector2 lookDirection = (_nextPos - _currentPos).normalized;
@@ -31,17 +47,18 @@ public class PhoneDirection : MonoBehaviour
 
         directionInDegrees = directionInDegrees * 180 / Mathf.PI;
 
-        directionInDegrees = (directionInDegrees - 90) * -1;
+        directionInDegrees = (directionInDegrees - 90);
 
         nextPointDebug.text = directionInDegrees.ToString();
 
-        directionInDegrees += headingDegrees;
-        
-        windrose.rotation = Quaternion.Euler(0, 0, directionInDegrees);
+        float nextPointDegrees = northHeadingDegrees + directionInDegrees;
 
-        // Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, headingDegrees));
-        //
-        // float rotationSpeed = (targetRotation.eulerAngles - windrose.eulerAngles).magnitude;
-        // windrose.rotation = Quaternion.RotateTowards(windrose.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        Quaternion windroseTarget = Quaternion.Euler(new Vector3(0, 0, northHeadingDegrees));
+        Quaternion pointerTarget = Quaternion.Euler(new Vector3(0, 0, nextPointDegrees));
+        
+        float windroseRotationSpeed = (windroseTarget.eulerAngles - windrose.eulerAngles).magnitude;
+        float pointerRotationSpeed = (pointerTarget.eulerAngles - pointer.eulerAngles).magnitude;
+        windrose.rotation = Quaternion.RotateTowards(windrose.rotation, windroseTarget, Time.deltaTime * windroseRotationSpeed);
+        pointer.rotation = Quaternion.RotateTowards(pointer.rotation, pointerTarget, Time.deltaTime * pointerRotationSpeed);
     }
 }
