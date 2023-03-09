d747fef3485bf82d68b66d2898a8fcd954098666
Calvin-Davidson <calvin2003davidson@gmail.com>
2023-03-09 10:20:43 +0100

    Strech removed, scale not fitting screen

--- BBKoffieTuin/Assets/Scripts/DeviceCamera.cs
+++ BBKoffieTuin/Assets/Scripts/DeviceCamera.cs
@@ -10,17 +10,49 @@ public class DeviceCamera : MonoBehaviour
     [SerializeField, Tooltip("Used to display the camera's texture")] private RawImage displayImage;
 
     private WebCamTexture _webCamTexture;
+    
+    public AspectRatioFitter imageFitter;
 
-    private void Awake()
+    //set it to either FRONT or BACK
+    string myCamera = "BACK";
+
+    // Device cameras
+    WebCamDevice frontCameraDevice;
+    WebCamDevice backCameraDevice;
+    WebCamDevice activeCameraDevice;
+
+    WebCamTexture frontCameraTexture;
+    WebCamTexture backCameraTexture;
+    WebCamTexture activeCameraTexture;
+
+    // Image rotation
+    Vector3 rotationVector = new Vector3(0f, 0f, 0f);
+
+    // Image uvRect
+    Rect defaultRect = new Rect(0f, 0f, 1f, 1f);
+    Rect fixedRect = new Rect(0f, 1f, 1f, -1f);
+
+    // Image Parent's scale
+    Vector3 defaultScale = new Vector3(1f, 1f, 1f);
+    Vector3 fixedScale = new Vector3(-1f, 1f, 1f);
+    
+
+    private void Start()
     {
         StartWebCam();
     }
 
     public void StartWebCam()
     {
-        _webCamTexture = new WebCamTexture(WebCamTexture.devices.FirstOrDefault(device => device.isFrontFacing).name);
+        string frontCamName = WebCamTexture.devices.FirstOrDefault(device => device.isFrontFacing).name;
+        _webCamTexture = new WebCamTexture(frontCamName);
         _webCamTexture.Play();
+
+        float scale = Math.Min(1920 / _webCamTexture.width, 1080 / _webCamTexture.height);
+        
+        
         displayImage.texture = _webCamTexture;
+        displayImage.rectTransform.sizeDelta = new Vector2(_webCamTexture.width * scale, _webCamTexture.height * scale);
     }
 
     public void TakePicture()
