de6c9c6db682a226304bbbc0a72da53e3d89ac14
Calvin-Davidson <calvin2003davidson@gmail.com>
2023-03-13 09:28:55 +0100

    Quiz system working

--- BBKoffieTuin/Assets/Scripts/Quiz/QuizManager.cs
+++ BBKoffieTuin/Assets/Scripts/Quiz/QuizManager.cs
@@ -14,11 +14,12 @@ namespace Quiz
         [SerializeField] private List<QuizQuestion> questions;
 
         private int _questionsDone = 0;
+        private int _correctAnswerCount = 0;
         private readonly List<QuizQuestion> _previousQuestions = new();
 
-        [Space]
-        public UnityEvent<QuizQuestion> onNewQuestion = new();
-        public UnityEvent onGameComplete = new();
+        [Space] public UnityEvent<QuizQuestion> onNewQuestion = new();
+        [Tooltip("Invoked when the game end, with the amount of correct answers as value")]
+        public UnityEvent<int> onGameComplete = new();
 
         public List<QuizQuestion> Questions => new(questions);
 
@@ -29,17 +30,39 @@ namespace Quiz
 
         private void DoNextQuestion()
         {
-            _questionsDone += 1;
-            if (_questionsDone > questionsToDo)
-            {
-                onGameComplete?.Invoke();
-            }
-
             QuizQuestion nextQuestions =
                 questions.Where(question => !_previousQuestions.Contains(question)).ToList().Shuffle().First();
-            
+
             _previousQuestions.Add(nextQuestions);
             onNewQuestion?.Invoke(nextQuestions);
+        }
+
+        public void HandleAnswerCorrect()
+        {
+            _questionsDone += 1;
+            _correctAnswerCount += 1;
+            if (CheckGameComplete()) return;
+
+            DoNextQuestion();
+        }
+
+        public void HandleAnswerIncorrect()
+        {
+            _questionsDone += 1;
+            if (CheckGameComplete()) return;
+
+            DoNextQuestion();
+        }
+
+        private bool CheckGameComplete()
+        {
+            if (_questionsDone == questionsToDo)
+            {
+                onGameComplete?.Invoke(_correctAnswerCount);
+                return true;
+            }
+
+            return false;
         }
     }
 }
--- BBKoffieTuin/Assets/Scripts/Quiz/QuizRenderer.cs
+++ BBKoffieTuin/Assets/Scripts/Quiz/QuizRenderer.cs
@@ -13,28 +13,44 @@ namespace Quiz
         [SerializeField] private TextMeshProUGUI questionText;
         [SerializeField] private List<Button> buttons;
         private QuizManager _quizManager;
+        private Button _correctButton;
 
         private void Awake()
         {
             _quizManager = FindObjectOfType<QuizManager>();
             _quizManager.onNewQuestion.AddListener(Render);
+            
+            buttons.ForEach(button => button.onClick.AddListener(() => HandleButtonPress(button)));
         }
 
         /// <summary>
         /// Render's the new quiz data, as we shuffle the buttons we can use slot[0] which is always in a random position.
         /// </summary>
         /// <param name="question">The new question we ask</param>
-        public void Render(QuizQuestion question)
+        private void Render(QuizQuestion question)
         {
             List<QuizQuestion> otherQuestions = _quizManager.Questions.Where(quizQuestion => question != quizQuestion).ToList().Shuffle();
             buttons = buttons.Shuffle();
             
             questionText.text = question.Question;
             buttons[0].GetComponent<Image>().sprite = question.Sprite;
+            _correctButton = buttons[0];
             
             for (int i = 1; i < buttons.Count; i++)
             {
                 buttons[i].GetComponent<Image>().sprite = otherQuestions[i-1].Sprite;
+            }
+        }
+
+        private void HandleButtonPress(Button button)
+        {
+            if (button == _correctButton)
+            {
+                _quizManager.HandleAnswerCorrect();
+            }
+            else
+            {
+                _quizManager.HandleAnswerIncorrect();
             }
         }
     }
